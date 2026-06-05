85c41328c7661a7414539feac9e56c1b845b4755
AishaA <aishaashirova23@gmail.com>
2026-06-05 09:35:41 -0400

    stuff we dont need

---
+++ endyear/character_body_3d.gd
@@ -0,0 +1,17 @@
+extends CharacterBody3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var speed = 5
+var jump_speed = 5
+var mouse_sensitivity = 0.002
+
+func _physics_process(delta):
+	velocity.y += -gravity * delta
+	var input = Input.get_vector("left", "right", "forward", "back")
+	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
+
+	move_and_slide()
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
